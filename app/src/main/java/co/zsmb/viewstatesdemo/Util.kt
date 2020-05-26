d9c7c631586105b053ffe2ae3bd694b594411723
Fred Porciúncula <fred.porciuncula@nufin.de>
2020-05-26 08:37:39 +0200

    Add LiveData select extension
    We’ll use it to be able to observe a single property of the ViewState. Inspired by MvRx’s `selectSubscribe` (https://github.com/airbnb/MvRx/blob/3e0924cfb1b3c080afc3f32f53934b7d959bc91a/mvrx/src/main/kotlin/com/airbnb/mvrx/MvRxView.kt#L80-L97) and based on the implementation by Jose Alcérreca here https://youtu.be/Iix75JjIbyI?t=1431

--- app/src/main/java/co/zsmb/viewstatesdemo/Util.kt
+++ app/src/main/java/co/zsmb/viewstatesdemo/Util.kt
@@ -1,3 +1,11 @@
 package co.zsmb.viewstatesdemo
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.distinctUntilChanged
+import androidx.lifecycle.map
+
 inline val <T> T.exhaustive get() = this
+
+inline fun <T, A> LiveData<T>.select(
+    crossinline property: T.() -> A
+) = map(property).distinctUntilChanged()
